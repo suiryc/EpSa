0fc5e3fe4fe6f9d7296326b2b05e47e6b5a5fc94
Julien Coloos <julien.coloos@gmail.com>
2015-07-08 23:38:39 +0200

    Cleanup code Use functions instead of EventHandler directly

--- build.sbt
+++ build.sbt
@@ -2,24 +2,30 @@ name := "EpSa"
 
 version := "1.0"
 
-val versions = Map[Symbol, String](
-  'akka      -> "2.3.11",
-  'akkaHttp  -> "1.0-RC4",
-  'logback   -> "1.1.2",
-  'poi       -> "3.12",
-  'scala     -> "2.11.7"
+val versions = Map[String, String](
+  "akka"         -> "2.3.11",
+  "akka-http"    -> "1.0-RC4",
+  "logback"      -> "1.1.2",
+  "poi"          -> "3.12",
+  "scala"        -> "2.11.7",
+  "suiryc-scala" -> "0.0.2-SNAPSHOT"
 )
 
-scalaVersion := versions('scala)
+scalaVersion := versions("scala")
 
 libraryDependencies ++= Seq(
-  "ch.qos.logback"    %  "logback-classic"                   % versions('logback),
-  "com.typesafe.akka" %% "akka-actor"                        % versions('akka),
-  "com.typesafe.akka" %% "akka-stream-experimental"          % versions('akkaHttp),
-  "com.typesafe.akka" %% "akka-http-core-experimental"       % versions('akkaHttp),
-  "com.typesafe.akka" %% "akka-http-experimental"            % versions('akkaHttp),
-  "com.typesafe.akka" %% "akka-http-spray-json-experimental" % versions('akkaHttp),
-  "com.typesafe.akka" %% "akka-slf4j"                        % versions('akka),
-  "org.apache.poi"    %  "poi"                               % versions('poi),
-  "org.apache.poi"    %  "poi-ooxml"                         % versions('poi)
+  "ch.qos.logback"    %  "logback-classic"                   % versions("logback"),
+  "com.typesafe.akka" %% "akka-actor"                        % versions("akka"),
+  "com.typesafe.akka" %% "akka-stream-experimental"          % versions("akka-http"),
+  "com.typesafe.akka" %% "akka-http-core-experimental"       % versions("akka-http"),
+  "com.typesafe.akka" %% "akka-http-experimental"            % versions("akka-http"),
+  "com.typesafe.akka" %% "akka-http-spray-json-experimental" % versions("akka-http"),
+  "com.typesafe.akka" %% "akka-slf4j"                        % versions("akka"),
+  "org.apache.poi"    %  "poi"                               % versions("poi"),
+  "org.apache.poi"    %  "poi-ooxml"                         % versions("poi"),
+  "suiryc"            %% "suiryc-scala-core"                 % versions("suiryc-scala"),
+  "suiryc"            %% "suiryc-scala-log"                  % versions("suiryc-scala"),
+  "suiryc"            %% "suiryc-scala-javafx"               % versions("suiryc-scala")
 )
+
+resolvers += Resolver.mavenLocal
--- src/main/scala/epsa/TestExcel.scala
+++ src/main/scala/epsa/TestExcel.scala
@@ -1,11 +1,10 @@
 package epsa
 
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Path, Paths}
 import java.text.SimpleDateFormat
 import java.util.Date
 import javafx.application.Application
-import javafx.event.EventHandler
-import javafx.geometry.{Bounds, Point2D}
+import javafx.geometry.Bounds
 import javafx.scene.{Node, Scene}
 import javafx.scene.chart.{XYChart, LineChart, NumberAxis, CategoryAxis}
 import javafx.scene.input.MouseEvent
@@ -15,13 +14,20 @@ import javafx.stage.Stage
 
 import org.apache.poi.ss.usermodel.WorkbookFactory
 
-object TestExcel extends App {
+import suiryc.scala.javafx.event.EventHandler._
 
-  val path = Paths.get("D:", "data.xls")
+object TestExcel {
 
-  val esalia = checkEsalia(path)
-  println(esalia)
-  esalia.foreach(displaySupport)
+  var support: Support = _
+
+  def main(args: Array[String]): Unit = {
+    val path = Paths.get("D:", "data.xls")
+
+    checkEsalia(path).foreach(support = _)
+    println(support)
+
+    (new TestExcel).launch()
+  }
 
   def checkEsalia(path: Path): Option[Support] = {
     Option(WorkbookFactory.create(path.toFile)).filter { book =>
@@ -65,25 +71,19 @@ object TestExcel extends App {
     }
   }
 
-  def displaySupport(support: Support): Unit = {
-    val test = new Test()
-    test.ttt(args, support)
-  }
-
 }
 
-object Test {
-  var support: Support = _
-}
+class TestExcel extends Application {
 
-class Test extends Application {
+  import TestExcel._
 
-  def ttt(args: Seq[String], support: Support) {
-    Test.support = support
-    Application.launch(args: _*)
+  var previousXPos: Option[String] = None
+
+  def launch() {
+    Application.launch()
   }
 
-  override def start(stage: Stage): Unit = {
+  override def start(stage: Stage) {
     val xAxis = new CategoryAxis()
     xAxis.setLabel("Date")
 
@@ -96,8 +96,8 @@ class Test extends Application {
     yAxis.setAutoRanging(true)
 
     val series = new XYChart.Series[String, Number]()
-    series.setName(Test.support.name)
-    val valuesList = Test.support.values.takeRight(100).map { v =>
+    series.setName(support.name)
+    val valuesList = support.values.takeRight(100).map { v =>
       (dateFormat.format(v.date), v.value)
     }
     val valuesMap = valuesList.toMap
@@ -137,51 +137,43 @@ class Test extends Application {
     // Note: it is not a good idea to track mouse from chartBackground, since
     // crossing any displayed element (e.g. grid) will trigger exited/entered.
     // Better track mouse on chart, and check whether it is over the graph.
-    chart.setOnMouseEntered(new EventHandler[MouseEvent]() {
-      override def handle(event: MouseEvent): Unit = {
-        // Note: we need to determine the position of the chart background
-        // relatively to the vertical line parent.
-        val bounds = getBounds(chartBackground, chartPane)
-        verticalLine.setStartY(bounds.getMinY)
-        verticalLine.setEndY(bounds.getMinY + bounds.getHeight)
-      }
-    })
-    chart.setOnMouseExited(new EventHandler[MouseEvent]() {
-      override def handle(event: MouseEvent): Unit = {
+    chart.setOnMouseEntered { (event: MouseEvent) =>
+      // Note: we need to determine the position of the chart background
+      // relatively to the vertical line parent.
+      val bounds = getBounds(chartBackground, chartPane)
+      verticalLine.setStartY(bounds.getMinY)
+      verticalLine.setEndY(bounds.getMinY + bounds.getHeight)
+    }
+    chart.setOnMouseExited { (event: MouseEvent) =>
+      verticalLine.setVisible(false)
+      horizontalLine.setVisible(false)
+    }
+    chart.setOnMouseMoved { (event: MouseEvent) =>
+      val bounds = getBounds(chartBackground, chartPane)
+
+      if (bounds.contains(event.getX, event.getY)) {
+        // Note: mouse position is relative to the chart, while xAxis works
+        // relatively to the background. So adjust.
+        Option(xAxis.getValueForDisplay(event.getX - bounds.getMinX)).foreach { xPos =>
+          if (!previousXPos.contains(xPos)) {
+            val x = bounds.getMinX + xAxis.getDisplayPosition(xPos)
+            val y = bounds.getMinY + yAxis.getDisplayPosition(valuesMap(xPos))
+            verticalLine.setStartX(x)
+            verticalLine.setEndX(x)
+            verticalLine.setVisible(true)
+            horizontalLine.setStartX(bounds.getMinX)
+            horizontalLine.setEndX(x)
+            horizontalLine.setStartY(y)
+            horizontalLine.setEndY(y)
+            horizontalLine.setVisible(true)
+          }
+          previousXPos = Some(xPos)
+        }
+      } else {
         verticalLine.setVisible(false)
         horizontalLine.setVisible(false)
       }
-    })
-    chart.setOnMouseMoved(new EventHandler[MouseEvent]() {
-      var previousXPos: Option[String] = None
-
-      override def handle(event: MouseEvent) {
-        val bounds = getBounds(chartBackground, chartPane)
-
-        if (bounds.contains(event.getX, event.getY)) {
-          // Note: mouse position is relative to the chart, while xAxis works
-          // relatively to the background. So adjust.
-          Option(xAxis.getValueForDisplay(event.getX - bounds.getMinX)).foreach { xPos =>
-            if (!previousXPos.contains(xPos)) {
-              val x = bounds.getMinX + xAxis.getDisplayPosition(xPos)
-              val y = bounds.getMinY + yAxis.getDisplayPosition(valuesMap(xPos))
-              verticalLine.setStartX(x)
-              verticalLine.setEndX(x)
-              verticalLine.setVisible(true)
-              horizontalLine.setStartX(bounds.getMinX)
-              horizontalLine.setEndX(x)
-              horizontalLine.setStartY(y)
-              horizontalLine.setEndY(y)
-              horizontalLine.setVisible(true)
-            }
-            previousXPos = Some(xPos)
-          }
-        } else {
-          verticalLine.setVisible(false)
-          horizontalLine.setVisible(false)
-        }
-      }
-    })
+    }
 
     val scene = new Scene(chartPane)
     stage.setScene(scene)
