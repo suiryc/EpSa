282f327a7eb53a262d147f5b7c2d87102b56ce73
Julien Coloos <julien.coloos@gmail.com>
2015-09-17 00:37:09 +0200

    Updated data store sandbox class

--- src/main/scala/epsa/sandbox/TestDataStore.scala
+++ src/main/scala/epsa/sandbox/TestDataStore.scala
@@ -2,11 +2,26 @@ package epsa.sandbox
 
 import epsa.model.Savings
 import epsa.storage.DataStore
-import scala.concurrent.Promise
+import javafx.application.Application
+import javafx.stage.Stage
+import scala.concurrent.{Await, Promise}
+import scala.concurrent.duration.Duration
 
 object TestDataStore {
 
   def main(args: Array[String]): Unit = {
+    (new TestDataStore).launch()
+  }
+
+}
+
+class TestDataStore extends Application {
+
+  def launch(): Unit = {
+    Application.launch()
+  }
+
+  override def start(stage: Stage) {
     import epsa.Main.Akka._
 
     val promise = Promise[Unit]()
@@ -15,6 +30,11 @@ object TestDataStore {
     }
 
     try {
+      if (DataStore.open(None, change = false).isEmpty) {
+        DataStore.open(None, change = true).foreach { r =>
+          Await.ready(r, Duration.Inf)
+        }
+      }
       DataStore.EventSource.readEvents().onComplete {
         case read =>
           println(s"EventSource.readEvents => $read")
