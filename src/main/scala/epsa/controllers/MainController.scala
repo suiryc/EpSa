b3c14e48f8ab527fb2cb1a3a096b4df0b72c7acd
Julien Coloos <julien.coloos@gmail.com>
2015-09-16 22:58:14 +0200

    Code cleanup

--- src/main/scala/epsa/controllers/MainController.scala
+++ src/main/scala/epsa/controllers/MainController.scala
@@ -16,7 +16,6 @@ import javafx.fxml.{FXML, FXMLLoader}
 import javafx.scene.{Parent, Scene}
 import javafx.scene.control._
 import javafx.stage._
-import javafx.stage.FileChooser.ExtensionFilter
 import suiryc.scala.RichOption._
 import suiryc.scala.concurrent.Callable
 import suiryc.scala.javafx.beans.value.RichObservableValue._
@@ -315,18 +314,17 @@ class MainController {
 
       val stage = new Stage()
 
-      val fundPathFolder = Preference.from("fund.path.folder", null:Path)
-      val fundPathFile = Preference.from("fund.path.file", null:String)
+      val fundPath = Preference.from("fund.path", null:Path)
 
       val fileChooser = new FileChooser()
       fileChooser.setTitle("Open Investment Fund File")
       fileChooser.getExtensionFilters.addAll(
-        new ExtensionFilter("Excel Files", "*.xls", "*.xlsx"),
-        new ExtensionFilter("All Files", "*.*")
+        new FileChooser.ExtensionFilter("Excel Files", "*.xls", "*.xlsx"),
+        new FileChooser.ExtensionFilter("All Files", "*.*")
       )
-      fundPathFolder.option.foreach { path =>
-        fileChooser.setInitialDirectory(path.toFile)
-        fundPathFile.option.foreach(fileChooser.setInitialFileName)
+      fundPath.option.foreach { path =>
+        fileChooser.setInitialDirectory(path.getParent.toFile)
+        fileChooser.setInitialFileName(path.toFile.getName)
       }
       val selectedFile = fileChooser.showOpenDialog(stage)
       Option(selectedFile).flatMap { file =>
@@ -334,8 +332,7 @@ class MainController {
       } match {
         case Some(fund) =>
           // Save path in preferences
-          fundPathFolder() = selectedFile.getParentFile.toPath
-          fundPathFile() = selectedFile.getName
+          fundPath() = selectedFile.toPath
           // Then build and display chart
           val chartHandler = new ChartHandler(fund)
           val chartPane = chartHandler.chartPane
