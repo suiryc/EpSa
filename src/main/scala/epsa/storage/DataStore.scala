a3879051ee3cdbe36935d57ed6a7f34a3769da7f
Julien Coloos <julien.coloos@gmail.com>
2015-09-16 23:05:26 +0200

    Data store selection refactoring
    Explicit method to select (changing or not) data store
    Returns result in a Future

--- src/main/scala/epsa/storage/DataStore.scala
+++ src/main/scala/epsa/storage/DataStore.scala
@@ -1,13 +1,13 @@
 package epsa.storage
 
+import epsa.I18N
 import epsa.model.Savings
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.sql.Timestamp
 import java.time.Instant
-import scala.concurrent.duration.Duration
-import scala.concurrent.Await
+import javafx.stage.{FileChooser, Window}
 import scala.concurrent.Future
 import slick.driver.H2Driver.api._
 import slick.driver.H2Driver.backend.DatabaseDef
@@ -20,46 +20,85 @@ object DataStore {
   import epsa.Main.prefs
   import Preference._
 
-  protected val dbPath = Preference.from("datastore.path", null:Path)
+  private val dbExtension = ".mv.db"
 
-  protected val defaultPath = dbPath.option.getOrElse {
+  protected val dbPathPref = Preference.from("datastore.path", null:Path)
+
+  protected val defaultPath = dbPathPref.option.getOrElse {
     val path = {
       // See: http://stackoverflow.com/a/12733172
       val appPath = Paths.get(getClass.getProtectionDomain.getCodeSource.getLocation.toURI)
       if (Files.isDirectory(appPath)) appPath
       else appPath.getParent
     }
-    path.resolve("default")
+    path.resolve(s"default$dbExtension")
   }
 
   protected case class DB(db: DatabaseDef, path: Path)
 
   protected var dbOpt: Option[DB] = None
 
-  /** Gets the DB (opening it if not done). */
+  /**
+   * Gets the DB.
+   *
+   * Throws an exception if DB is not opened.
+   * Caller is responsible to ensure DB has been opened beforehand.
+   */
   protected def getDB: DB =
     dbOpt match {
       case Some(v) => v
-      case None    => Await.result(dbOpen(defaultPath), Duration.Inf)
+      case None    => throw new Exception(I18N.getResources.getString("No data store selected"))
     }
 
-  def changePath(newPath: Path): Future[Unit] = {
-    val oldDB = getDB
-    val oldPath = oldDB.path
-    if (newPath.compareTo(oldPath) != 0) {
+  def open(owner: Window, change: Boolean): Option[Future[Unit]] = {
+    val resources = I18N.getResources
+
+    if (change) {
+      val fileChooser = new FileChooser()
+      fileChooser.setTitle(resources.getString("Select data store"))
+      fileChooser.getExtensionFilters.addAll(
+        new FileChooser.ExtensionFilter(resources.getString("Data store"), "*.mv.db")
+      )
+      fileChooser.setInitialDirectory(defaultPath.getParent.toFile)
+      fileChooser.setInitialFileName(defaultPath.toFile.getName)
+
+      // TODO - prevent "Overwrite existing file" confirmation if file exists ?
+      val selectedFile = fileChooser.showSaveDialog(owner)
+      Option(selectedFile).map { file =>
+        changePath(file.toPath).map(_ => ())
+      }
+    }
+    else if (Files.exists(defaultPath)) {
+      Some(changePath(defaultPath).map(_ => ()))
+    } else {
+      None
+    }
+  }
+
+  def close(): Unit = {
+    dbOpt.foreach(_.db.close())
+    dbOpt = None
+  }
+
+  protected def changePath(newPath: Path): Future[DB] = {
+    if (!dbOpt.map(_.path).exists(_.compareTo(newPath) == 0)) {
       try {
-        val r = dbOpen(newPath).map(_ => ())
-        oldDB.db.close()
-        r
+        close()
+        dbOpen(newPath)
       } catch {
         case ex: Throwable => Future.failed(ex)
       }
-    } else Future.successful(())
+    } else Future.successful(dbOpt.get)
   }
 
   protected def dbOpen(path: Path): Future[DB] = {
+    // Note: path contains extension, which is added by driver.
+    val name0 = path.toFile.getName
+    val name = name0.substring(0, name0.length - dbExtension.length)
+    val dbPath = path.getParent.resolve(name)
+
     // Open the new DB, and create missing tables
-    val refNew = Database.forURL(s"jdbc:h2:$path", user = "user", password = "pass", driver = "org.h2.Driver")
+    val refNew = Database.forURL(s"jdbc:h2:$dbPath", user = "user", password = "pass", driver = "org.h2.Driver")
 
     refNew.run(MTable.getTables(EventSource.tableName)).flatMap { tables =>
       if (tables.nonEmpty) Future.successful(())
@@ -70,7 +109,7 @@ object DataStore {
       dbOpt = Some(dbNew)
 
       // Success, so save path
-      dbPath() = path
+      dbPathPref() = path
 
       dbNew
     }
