a276ae98490c816d4f2f4c375f3d9d6900c96b10
Julien Coloos <julien.coloos@gmail.com>
2019-10-30 21:49:59 +0100

    Updated to match latest Esalia site changes
    URL, HTTP request to extract fund history values, and returned document
    content changed.

--- src/main/scala/epsa/tools/AmundiInvestmentFundDownloader.scala
+++ src/main/scala/epsa/tools/AmundiInvestmentFundDownloader.scala
@@ -4,9 +4,7 @@ import epsa.model.Savings
 import epsa.util.Http
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import org.htmlcleaner.{HtmlCleaner, TagNode}
 import scala.concurrent.Future
-import scala.jdk.CollectionConverters._
 
 /**
  * Amundi investment fund file downloader.
@@ -15,78 +13,30 @@ object AmundiInvestmentFundDownloader {
 
   import epsa.Main.Akka._
 
-  /** Tag id where to find the 'download' form. */
-  private val FORM_ID = "rub-66"
-  /** Tag id for form date start. */
-  private val FORM_DATE_START_ID = "datepicker_debut_vl"
-  /** Tag id for form date end. */
-  private val FORM_DATE_END_ID = "datepicker_fin_vl"
   /** Date formatter. */
-  private val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+  private val dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd")
 
   def download(amfId: String, dateStart: Option[LocalDate] = None,
     dateEnd: Option[LocalDate] = None): Future[Savings.AssetValueHistory] =
   {
-    val uri = getUri(amfId)
-
     val start = dateStart.getOrElse(LocalDate.of(1970, 1, 1))
     val end = dateEnd.getOrElse(LocalDate.now)
-    getFormParameters(uri, start, end).map { formParameters =>
-      val body = Http.executeBinaryRequest(Http.buildHttpPost(uri.toString, formParameters.toSeq))
+    val uri = getUri(amfId, start, end)
+    Future {
+      val body = Http.executeBinaryRequest(Http.buildHttpGet(uri.toString))
       EsaliaInvestmentFundProber.probe(body).getOrElse {
         throw new Exception("Downloaded data could not be probed")
       }
     }
   }
 
-  private def getUri(amfId: String): String =
-    s"http://sggestion-ede.com/product/index.php?amf=$amfId&doc=fp"
-
-  private def getFormParameters(uri: String, dateStart: LocalDate, dateEnd: LocalDate): Future[Map[String, String]] = {
-    Future {
-      val body = Http.executeTextRequest(Http.buildHttpGet(uri))
-      val parameters = extractFormParameters((new HtmlCleaner).clean(body))
-
-      parameters ++ Map(
-        FORM_DATE_START_ID -> dateFormatter.format(dateStart),
-        FORM_DATE_END_ID -> dateFormatter.format(dateEnd)
-      )
-    }
-  }
-
-  private def extractFormParameters(root: TagNode): Map[String, String] = {
-    val form = root.findElementByAttValue("id", FORM_ID, true, false)
-    if (Option(form).isEmpty) {
-      throw new Exception(s"Site does not contain expected form inside $FORM_ID element")
-    }
-
-    val inputs = form.getElementListByName("input", true).asScala.filter { node =>
-      node.hasAttribute("name") && node.hasAttribute("value")
-    }.map { node =>
-      node.getAttributeByName("name") -> node.getAttributeByName("value")
-    }.toMap
-
-    val selects = form.getElementListByName("select", true).asScala.filter { node =>
-      node.hasAttribute("name")
-    }.flatMap { node =>
-      node.getElementListByName("option", false).asScala.find { option =>
-        option.hasAttribute("value") && option.hasAttribute("SELECTED")
-      }.map { option =>
-        node.getAttributeByName("name") -> option.getAttributeByName("value")
-      }
-    }.toMap
-
-    val parameters = inputs ++ selects
-    def checkParameter(id: String): Unit = {
-      if (parameters.get(id).isEmpty) {
-        throw new Exception(s"Site form does not contain expected input $id element")
-      }
-    }
-
-    checkParameter(FORM_DATE_START_ID)
-    checkParameter(FORM_DATE_END_ID)
-
-    parameters
-  }
+  private def getUri(amfId: String, dateStart: LocalDate, dateEnd: LocalDate): String =
+    s"https://www.societegeneralegestion.fr/psSGGestionEntr/ezaap/service/ExportDataProductSheet/Fwd/fr-FR/886/${
+      amfId
+    }/object/export/graphiquevl?bDate=${
+      dateFormatter.format(dateStart)
+    }&eDate=${
+      dateFormatter.format(dateEnd)
+    }&devise=3&duration=&perPage=0"
 
 }
--- src/main/scala/epsa/tools/EsaliaInvestmentFundProber.scala
+++ src/main/scala/epsa/tools/EsaliaInvestmentFundProber.scala
@@ -34,36 +34,40 @@ object EsaliaInvestmentFundProber extends InvestmentFundProber {
   }
 
   private def probeExcel(book: Workbook): Option[Savings.AssetValueHistory] = {
-    // Rows 0 to 5 contain general information or nothing
-    // Row 1 contains the investment fund name
-    // Row 5 indicates which data are listed: cell 4 shall contain the date
-    // and cell 5 the value at the given date
-    // Rows 6 and beyond contain dated values
-    val nameRowIdx = 1
-    val labelsRowIdx = 5
+    // Rows 0 to 4 contain nothing
+    // Rows 5 to 13 contain information on fund and extraction request
+    // Row 7 contains the investment fund name
+    // Row 15 indicates which data are listed: cell 1 shall contain the date
+    // and cell 2 the value at the given date
+    // Rows 16 and beyond contain dated values
+    // Cell 0 is empty
+    val firstRowIdx = 5
+    val firstCellIdx = 1
+    val nameRowIdx = 7
+    val labelsRowIdx = 15
     val dataRowIdx = labelsRowIdx + 1
-    val dateCellIdx = 4
-    val valueCellIdx = 5
+    val dateCellIdx = 1
+    val valueCellIdx = 2
     Some(book).filter { book =>
-      // There should only be one sheet
-      book.getNumberOfSheets == 1
+      // There must be at least one sheet
+      book.getNumberOfSheets >= 1
     }.map { book =>
       book.getSheetAt(0)
     }.filter { sheet =>
-      sheet.getSheetName.startsWith("Historique des valeurs liquida") &&
-        (sheet.getFirstRowNum == 0) && (sheet.getLastRowNum >= dataRowIdx)
+      sheet.getSheetName.startsWith("Historique des VL") &&
+        (sheet.getFirstRowNum == firstRowIdx) && (sheet.getLastRowNum >= dataRowIdx)
     }.flatMap { sheet =>
       val nameRow = sheet.getRow(nameRowIdx)
       val labelsRow = sheet.getRow(labelsRowIdx)
 
-      if ((nameRow.getFirstCellNum == 0) && (nameRow.getLastCellNum >= 1) &&
-        (nameRow.getCell(0).getStringCellValue == "Nom du fonds") &&
-        (labelsRow.getFirstCellNum == 0) && (labelsRow.getLastCellNum >= math.max(dateCellIdx, valueCellIdx)) &&
-        (labelsRow.getCell(dateCellIdx).getStringCellValue == "Date VL") &&
-        (labelsRow.getCell(valueCellIdx).getStringCellValue == "VL"))
+      if ((nameRow.getFirstCellNum == firstCellIdx) && (nameRow.getLastCellNum > firstCellIdx) &&
+        (nameRow.getCell(firstCellIdx).getStringCellValue == "Nom du fonds") &&
+        (labelsRow.getFirstCellNum == firstCellIdx) && (labelsRow.getLastCellNum >= math.max(dateCellIdx, valueCellIdx)) &&
+        (labelsRow.getCell(dateCellIdx).getStringCellValue == "Dates") &&
+        (labelsRow.getCell(valueCellIdx).getStringCellValue == "VL(C)"))
       {
         val dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
-        val name = nameRow.getCell(1).getStringCellValue
+        val name = nameRow.getCell(firstCellIdx + 1).getStringCellValue
         val values = (dataRowIdx to sheet.getLastRowNum).toList.map { rowIdx =>
           val row = sheet.getRow(rowIdx)
           Savings.AssetValue(
